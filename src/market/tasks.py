4742c257484900a43e1f5c4b7f3dba442c70b3c0
Sahil Mehdiyev <branchsahil@gmail.com>
2025-08-15 14:51:28 +0400

    feat: Sync Stock Data Functions

---
+++ src/market/tasks.py
@@ -0,0 +1,40 @@
+from datetime import timedelta, timezone
+from django.apps import apps
+from .utils import batch_insert_stock_data
+import helpers.clients as helpers_clients
+
+
+def sync_company_stock_quotes(company_id, days_ago=32, 
+                              date_format = '%Y-%m-%d', verbose=False):
+    Company = apps.get_model('market', 'Company')
+    try:
+        company_obj = Company.objects.get(id=company_id)
+    except: company_obj = None
+    if company_obj is None:
+        raise Exception(f'{company_id} invalid')
+    company_ticker = company_obj.ticker
+    if company_ticker is None:
+        raise Exception(f'{company_ticker} invalid')
+    
+    end_date = timezone.now()
+    start_date = end_date - timedelta(days = days_ago)
+    to_date = end_date.strftime(date_format)
+    from_date = start_date.strftime(date_format)
+    client = helpers_clients.PolygonAPIClient(
+        ticker=company_ticker,
+        from_date=from_date,
+        to_date=to_date
+    )
+    dataset = client.get_stock_data()
+    if verbose:
+        print('dataset length', len(dataset))
+    batch_insert_stock_data(dataset=dataset,
+                            company_obj=company_obj, verbose=verbose)
+
+
+def sync_stock_data():
+    Company = apps.get_model('market', 'Company')
+    companies = Company.objects.filter(active=True).values_list('id')
+    for company_id in companies:
+        sync_company_stock_quotes(company_id)
+    
